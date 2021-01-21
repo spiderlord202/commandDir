f2a2affb1eb5fbca17e45c01fc2d68ad28248655
spider_under_your_bed <71126384+spiderlord202@users.noreply.github.com>
2021-01-21 21:59:20 +1300

    Update help.js

--- InDevelopment/help.js
+++ InDevelopment/help.js
@@ -1,1 +1,15 @@
 const Discord = require("discord.js")
+
+module.exports = {
+  name: "help",
+  description: "help",
+  execute: async (bot, message, args, split) => {
+    for (i = 0; i < bot.AvalibleCommands.length; i++) {
+     //bot.commands.get(bot.AvalibleCommands[i])
+     async function Await(){
+      await message.channel.send(bot.AvalibleCommands[i])
+     }
+     Await()
+  }
+}
+}
