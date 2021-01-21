8c5f5971c03af4a3579db527b6790550976fd2ed
spider_under_your_bed <71126384+spiderlord202@users.noreply.github.com>
2021-01-21 19:50:36 +1300

    Create creator.js

---
+++ creator.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "creator",
+  description: "shows them the creator",
+  execute: async (bot, message, args, split) => {
+    message.channel.send("My creator is Spider_Under_Your_Bed#8060");
+  }
+};
