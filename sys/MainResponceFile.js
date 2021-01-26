96f08bbbf88b56991ba010dfb2a1e8732845b0b8
spider_under_your_bed <71126384+spiderlord202@users.noreply.github.com>
2021-01-26 16:35:28 +1300

    Update MainResponceFile.js

--- sys/MainResponceFile.js
+++ sys/MainResponceFile.js
@@ -1,43 +1,5 @@
-const Pcall = new Set();
-//for stuff limmited to certain servers and the chatbot aspect
-const Responces = [
-  "e",
-  "bruh",
-  "why",
-  "suck",
-  "why u do this",
-  "f",
-  "how about no u",
-  "no u",
-  "hi",
-  "how old are you?",
-  "i was not talking to you",
-  "is this chatbot?"
-];
-const Responce = [
-  "E",
-  "bruh",
-  "why",
-  "suck marios a p####",
-  "life is not fair so suffer",
-  "R.I.P",
-  "https://tenor.com/view/card-no-u-wave-uno-card-gif-15455638",
-  "https://tenor.com/view/no-u-keanu-reeves-got-it-gif-14479787",
-  "hi welcome to chillys",
-  "i am ** years old",
-  ";(",
-  "Yes. I have been enabled to respond to messages without my prefix by my bot developer or this channel has BOT (in all caps) in the topic. I have a limmited number of reponces and I DO NOT learn from what you say."
-];
 
 
-async function CycleResponces(message){
-for (const i in Responces) {
-      if (message.content.toLowerCase() == Responces[i]) {
-      message.channel.send(Responce[i]);
-      Pcall.add(message.author.id);
-    }
-  }
-}
 //
 //Pcall.clear()
 //
@@ -50,7 +12,45 @@ module.exports = {
   name: "ReponceFile",
   description:
     "Sends a reponce to a non command message said in a certain channel or does things limmited to certain servers",
-  execute: async message => {
+  execute: async message => { 
+    const Pcall = new Set();
+    //for stuff limmited to certain servers and the chatbot aspect
+   const Responces = [
+     "e",
+     "bruh",
+     "why",
+     "suck",
+     "why u do this",
+     "f",
+     "how about no u",
+     "no u",
+     "hi",
+     "how old are you?",
+     "i was not talking to you",
+     "is this a chatbot?"
+  ];
+   const Responce = [
+    "E",
+    "bruh",
+    "why",
+    "suck marios a p####",
+    "life is not fair so suffer",
+    "R.I.P",
+    "https://tenor.com/view/card-no-u-wave-uno-card-gif-15455638",
+    "https://tenor.com/view/no-u-keanu-reeves-got-it-gif-14479787",
+    "hi welcome to chillys",
+    "i am ** years old",
+    ";(",
+    "Yes. I have been enabled to respond to messages without my prefix by my bot developer or this channel has BOT (in all caps) in the topic. I have a limmited number of reponces and I DO NOT learn from what you say."
+  ];
+    async function CycleResponces(message){
+     for (const i in Responces) {
+      if (message.content.toLowerCase() == Responces[i]) {
+      message.channel.send(Responce[i]);
+      Pcall.add(message.author.id);
+     }
+   }
+  }
     async function LoopBackArray() {
       const Ids = ["726982488530092134"]
        const condition = (ids) => ids == message.channel.id;
